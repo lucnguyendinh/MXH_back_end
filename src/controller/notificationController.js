df62e2007f0667bd277ec5acc467100f3c2e3e1e
lucnguyendinh <lucnguyendinh202@gmail.com>
2023-07-10 23:00:25 +0700

    fix notification

--- src/controller/notificationController.js
+++ src/controller/notificationController.js
@@ -1,4 +1,4 @@
-const { Notification } = require('../models')
+const { Notification, Status } = require('../models')
 
 const notificationController = {
     notification: async (req, res) => {
@@ -14,50 +14,58 @@ const notificationController = {
 
     like: async (req, res) => {
         try {
+            const status = await Status.findById(req.body.idStatus)
+            if (status.user == req.body.idOther) {
+                return res.status(200).json('Status cua ban...')
+            }
             const check = await Notification.findOne({
                 idStatus: req.body.idStatus,
                 action: 1,
                 idOther: { $in: [req.body.idOther] },
             })
-            const status = await Notification.findOne({ idStatus: req.body.idStatus, action: 1 })
+            const statusNotifi = await Notification.findOne({ idStatus: req.body.idStatus, action: 1 })
 
             if (check) {
                 return res.status(400).json('notification: Ban da like bai nay r...')
             }
             //neu k co ai like
-            if (!status) {
+            if (!statusNotifi) {
                 const likeNotifi = new Notification({
                     idUser: req.body.idUser,
                     idOther: req.body.idOther,
                     idStatus: req.body.idStatus,
                     action: 1,
                 })
-                await likeNotifi.save()
-                return res.status(200).json('notification: like')
+                const a = await likeNotifi.save()
+                return res.status(200).json('notification: like a')
             }
-            await status.updateOne({
+            const b = await statusNotifi.updateOne({
                 $push: { idOther: req.body.idOther },
-                count: status.count + 1,
+                count: statusNotifi.count + 1,
             })
-            return res.status(200).json('notification: like')
+            return res.status(200).json('notification: like b')
         } catch (err) {
             return res.status(500).json(err)
         }
     },
     unLike: async (req, res) => {
         try {
-            const status = await Notification.findOne({
+            const status = await Status.findById(req.body.idStatus)
+            if (status.user == req.body.idOther) {
+                return res.status(200).json('Status cua ban...')
+            }
+            const statusNotifi = await Notification.findOne({
                 idStatus: req.body.idStatus,
                 action: 1,
                 idOther: { $in: [req.body.idOther] },
             })
 
-            if (!status) {
+            if (!statusNotifi) {
                 return res.status(400).json('notification: Ban chua like bai viet nay...')
             }
-            await status.updateOne({
+            await statusNotifi.updateOne({
                 $pull: { idOther: req.body.idOther },
-                count: status.count - 1,
+                count: statusNotifi.count - 1,
             })
 
             return res.status(200).json('notification: Da xoa')
@@ -67,8 +75,12 @@ const notificationController = {
     },
     comment: async (req, res) => {
         try {
-            const status = await Notification.findOne({ idStatus: req.body.idStatus, action: 2 })
-            if (!status) {
+            const status = await Status.findById(req.body.idStatus)
+            if (status.user == req.body.idOther) {
+                return res.status(200).json('Status cua ban...')
+            }
+            const statusNotifi = await Notification.findOne({ idStatus: req.body.idStatus, action: 2 })
+            if (!statusNotifi) {
                 const commentNotifi = new Notification({
                     idUser: req.body.idUser,
                     idOther: req.body.idOther,
@@ -78,19 +90,48 @@ const notificationController = {
                 await commentNotifi.save()
                 return res.status(200).json('notification: comment')
             }
-            await status.updateOne({
+            await statusNotifi.updateOne({
                 $push: { idOther: req.body.idOther },
-                count: status.count + 1,
+                count: statusNotifi.count + 1,
             })
             return res.status(200).json('notification: comment')
         } catch (err) {
             return res.status(500).json(err)
         }
     },
+    unComment: async (req, res) => {
+        try {
+            const status = await Status.findById(req.body.idStatus)
+            if (status.user == req.body.idOther) {
+                return res.status(200).json('Status cua ban...')
+            }
+            const statusNotifi = await Notification.findOne({ idStatus: req.body.idStatus, action: 2 })
+            // await status.updateOne({
+            //     $pull: { idOther: { $elemMatch: { $eq: req.body.idOther } } },
+            //     count: status.count - 1,
+            // })
+            if (statusNotifi) {
+                const array = statusNotifi.idOther
+                const lastIndex = array.indexOf(req.body.idOther)
+
+                if (lastIndex !== -1) {
+                    array.splice(lastIndex, 1)
+                    await statusNotifi.updateOne({ $set: { idOther: array }, count: statusNotifi.count - 1 })
+                }
+            }
+            return res.status(200).json('notification: Da xoa')
+        } catch (err) {
+            return res.status(500).json(err)
+        }
+    },
     share: async (req, res) => {
         try {
-            const status = await Notification.findOne({ idStatus: req.body.idStatus, action: 3 })
-            if (!status) {
+            const status = await Status.findById(req.body.idStatus)
+            if (status.user == req.body.idOther) {
+                return res.status(200).json('Status cua ban...')
+            }
+            const statusNotifi = await Notification.findOne({ idStatus: req.body.idStatus, action: 3 })
+            if (!statusNotifi) {
                 const shareNotifi = new Notification({
                     idUser: req.body.idUser,
                     idOther: req.body.idOther,
@@ -100,9 +141,9 @@ const notificationController = {
                 await shareNotifi.save()
                 return res.status(200).json('notification: share')
             }
-            await status.updateOne({
+            await statusNotifi.updateOne({
                 $push: { idOther: req.body.idOther },
-                count: status.count + 1,
+                count: statusNotifi.count + 1,
             })
             return res.status(200).json('notification: share')
         } catch (err) {
@@ -110,33 +151,16 @@ const notificationController = {
         }
     },
 
-    unComment: async (req, res) => {
-        try {
-            const status = await Notification.findOne({ idStatus: req.body.idStatus, action: 2 })
-            // await status.updateOne({
-            //     $pull: { idOther: { $elemMatch: { $eq: req.body.idOther } } },
-            //     count: status.count - 1,
-            // })
-            if (status) {
-                const array = status.idOther
-                const lastIndex = array.indexOf(req.body.idOther)
-
-                if (lastIndex !== -1) {
-                    array.splice(lastIndex, 1)
-                    await status.updateOne({ $set: { idOther: array }, count: status.count - 1 })
-                }
-            }
-            return res.status(200).json('notification: Da xoa')
-        } catch (err) {
-            return res.status(500).json(err)
-        }
-    },
     unShare: async (req, res) => {
         try {
-            const status = await Notification.findOne({ idStatus: req.body.idStatus, action: 3 })
-            await status.updateOne({
+            const status = await Status.findById(req.body.idStatus)
+            if (status.user == req.body.idOther) {
+                return res.status(200).json('Status cua ban...')
+            }
+            const statusNotifi = await Notification.findOne({ idStatus: req.body.idStatus, action: 3 })
+            await statusNotifi.updateOne({
                 $pull: { idOther: req.body.idOther },
-                count: status.count - 1,
+                count: statusNotifi.count - 1,
             })
             return res.status(200).json('notification: Da xoa')
         } catch (err) {
